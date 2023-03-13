91577524bcfde2691b6694b93736d2ad9bc9a49c
irek <a_irek@inbox.ru>
2023-03-13 22:57:34 +0500

    copy filter source

--- src/app/table/services/table-source.service.ts
+++ src/app/table/services/table-source.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Column } from '../model/column';
 import { ServerSideSource } from '../model/server-side-source';
+import { Source } from '../model/source';
 import { TableDataStoreService } from './table-data-store.service';
 
 @Injectable()
 export class TableSourceService {
+  private sources: Record<number, Source> = {};
   constructor(private dataStore: TableDataStoreService) {}
 
   buildTableSource(tableId: number, pageSize: number) {
     const data = this.dataStore.get(tableId);
-    return new ServerSideSource(data, pageSize);
+    const source = new ServerSideSource(data, pageSize);
+    this.sources[tableId] = source;
+    return source;
   }
 
   buildFilterSource(tableId: number, pageSize: number, column: Column) {
-    const data = this.dataStore.get(tableId).map((value) => {
-      let o: any = {};
-      o[column.column] = value[column.column];
-      return o;
-    });
-    return new ServerSideSource(data, pageSize, true);
+    const data = this.dataStore.get(tableId)
+    const source = new ServerSideSource(data, pageSize, true) 
+    const originTableSource = this.sources[tableId];
+    if (originTableSource && originTableSource instanceof ServerSideSource) {
+      source.filterState = originTableSource.filterState
+    }
+    return source;
   }
 }
