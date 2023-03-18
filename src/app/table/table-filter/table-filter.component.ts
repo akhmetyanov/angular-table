2038c4fcf10be940b2afad34ab6d5c0c66fa6ee8
irek <a_irek@inbox.ru>
2023-03-18 11:24:29 +0500

    added clear filter command

--- src/app/table/table-filter/table-filter.component.ts
+++ src/app/table/table-filter/table-filter.component.ts
@@ -47,4 +47,8 @@ export class TableFilterComponent implements OnInit {
   onSelectValue() {
     this.filterEventService.registerEvent(this.tableId, this.column, this.selectedValues)
   }
+
+  onClearClick() {
+    this.filterEventService.registerEvent(this.tableId, this.column, [])
+  }
 }
